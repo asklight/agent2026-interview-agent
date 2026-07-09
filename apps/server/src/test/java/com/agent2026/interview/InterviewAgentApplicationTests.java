ae95cd21dd80f571d4bbdd4325e6d70a2c073fa1
asklight <13974300+asklight@user.noreply.gitee.com>
2026-07-09 21:55:40 +0800

    Initialize Spring Boot server skeleton

---
+++ apps/server/src/test/java/com/agent2026/interview/InterviewAgentApplicationTests.java
@@ -0,0 +1,12 @@
+package com.agent2026.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
